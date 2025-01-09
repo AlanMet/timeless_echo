d20fc72d6a004b00814c0743f9c91a6e80ad6cc1
AlanMet <106497267+AlanMet@users.noreply.github.com>
2025-01-09 22:47:41 +0000

    Refactor TextInput to use Provider for state management, enhancing input handling and UI responsiveness

--- lib/ui/input.dart
+++ lib/ui/input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timeless_echo/notifier.dart';
+import 'package:provider/provider.dart';
 
 class TextInput extends StatefulWidget {
   const TextInput({super.key});
@@ -11,16 +12,13 @@ class TextInput extends StatefulWidget {
 class _TextInputState extends State<TextInput> {
   final TextEditingController textController = TextEditingController();
   final _focusNode = FocusNode();
-  Controller controller = Controller();
 
   void _processInput() {
     String input = textController.text.trim();
     textController.clear();
     if (input.isNotEmpty) {
       List<String> commands = input.split(' ');
-      print("Processing commands : $commands");
       Controller().game.processCommand(commands);
-      textController.clear();
     }
   }
 
@@ -28,64 +26,50 @@ class _TextInputState extends State<TextInput> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
-      child: Expanded(
-        child: TextField(
-          controller: textController,
-          focusNode: _focusNode,
-          onSubmitted: (value) {
-            _processInput();
-            _focusNode.requestFocus();
-          },
-          decoration: InputDecoration(
-            hintText: 'Enter command',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(
-                color: controller
-                    .theme.theme.colorScheme.onSurface, // Default border color
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(
-                color: controller
-                    .theme.theme.colorScheme.primary, // Focused border color
-              ),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(
-                color: controller.theme.theme.colorScheme
-                    .onSurface, // Border color when enabled
-              ),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(
-                color: controller
-                    .theme.theme.colorScheme.error, // Error state border color
-              ),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(
-                color: controller.theme.theme.colorScheme
-                    .error, // Error state border when focused
-              ),
-            ),
-            suffixIcon: IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () {
+      child: Consumer<Controller>(
+        builder: (context, controller, child) {
+          return Expanded(
+            child: TextField(
+              controller: textController,
+              focusNode: _focusNode,
+              onSubmitted: (value) {
                 _processInput();
                 _focusNode.requestFocus();
               },
+              decoration: InputDecoration(
+                hintText: 'Enter command',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: controller.theme.theme.colorScheme.onSurface,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: controller.theme.theme.colorScheme.primary,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: controller.theme.theme.colorScheme.onSurface,
+                  ),
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {
+                    _processInput();
+                    _focusNode.requestFocus();
+                  },
+                ),
+              ),
+              style: TextStyle(
+                color: controller.theme.theme.colorScheme.onSurface,
+              ),
             ),
-          ),
-          style: TextStyle(
-            color: controller.theme.theme.colorScheme
-                .onSurface, // Text color matches onSurface color
-          ),
-        ),
+          );
+        },
       ),
     );
   }
